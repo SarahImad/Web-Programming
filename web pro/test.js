b22c3fac544a27b9d3afa1a3f9c3a3e603637f82
SarahImad <Saraiafham20330@gmail.com>
2024-07-01 21:17:22 +0400

    class 1

---
+++ web pro/test.js
@@ -0,0 +1,31 @@
+/*let user = null
+console.log(user)
+console.log(typeof user)
+console.log("hello")*/
+
+/*x = 5 //assigning 5 to a variable.
+console.log(x)
+
+console.log(typeof x)*/
+
+/*let x = 5 //here we used let as a good practice (var can be used too)
+let _taha = 8
+x = _taha
+console.log(x)
+
+console.log(typeof x)*/
+//
+/*
+let r = 5 
+let a
+const pi = 3.14 // cannot change later 
+a = pi*r*r
+console.log(a)
+console.log(typeof r/"s")
+*/
+
+let f = "sara"
+let s = 'I am "sara"'
+console.log(s)
+
+
